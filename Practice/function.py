2c86843b4e19b5c5ff0fea1e854ff42e49bf85b9
anishtamanggg <atamang@proclivitysystems.com>
2023-05-18 11:23:30 -0400

    practice codes

---
+++ Practice/function.py
@@ -0,0 +1,22 @@
+
+# def Add_Function(x,y):
+#     print(x+y)
+
+# # print(Add_Function(1,2))
+# Add_Function(1,2)
+
+# n=2
+# def my_function():
+#     n=3 
+#     print(n)
+
+# my_function()
+
+
+def divide(n1, n2): 
+    result = n1 / n2
+    return result
+
+print(divide(10,5))
+
+print(divide(n2=1,n1=5))
---
+++ Practice/main.py
@@ -0,0 +1,20 @@
+# print("Hello " + input("What is your name? : "))
+
+Anish_Age=26
+print(Anish_Age)
+
+# Anish_Age +=10
+#Anish_Age = Anish_Age +10
+
+print(Anish_Age)
+print(Anish_Age+3.3)
+
+
+print(f"Anish is {Anish_Age} years old")
+
+Anish_Age=float(Anish_Age)
+
+print(Anish_Age)
+print(type(Anish_Age))
+
+print(Anish_Age+3.3)
